71bec2ebd9d20d6242576e13ebe3b39928b30528
giovaniavila <giovanicarvalhoavila@hotmail.com>
2024-04-26 16:39:12 -0300

    feat: adiciona tela de historico

--- client/src/pages/Historico.tsx
+++ client/src/pages/Historico.tsx
@@ -1,13 +1,19 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Heading } from "@chakra-ui/react";
+import { Divider } from "@chakra-ui/react";
 import React from "react";
 
 const Historico = () => {
   return (
-    <Box display="grid" width="100vw">
-      <Text>
-        Essa é a página do
-        históssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssrico
-      </Text>
+    <Box display="grid" width="100vw" padding="6rem" >
+      <Box>
+        <Heading as="h1" fontWeight="500" marginBottom=".6rem">
+           Últimas agendas
+        </Heading>
+        <Divider width="100%" marginBottom="1rem"/>
+        <Box overflowY="auto" >
+          {/* {renderizar os cards aqui com map} */}
+        </Box>
+      </Box>
     </Box>
   );
 };
